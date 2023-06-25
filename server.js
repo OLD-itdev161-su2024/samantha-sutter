f6429bbd05e0a9c6b902d66f3fdb5956961bbc6e
SamanthaSutter <111908649+SamanthaSutter@users.noreply.github.com>
2023-06-25 16:37:02 -0500

    .

--- server.js
+++ server.js
@@ -42,4 +42,4 @@ app.post('/api/users',
 });
 
 //Connection listener
-app.listen(3000, () => console.log('Express server running on port 3000'));lalala
+app.listen(3000, () => console.log('Express server running on port 3000'));
