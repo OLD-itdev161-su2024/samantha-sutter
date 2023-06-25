adff6b0bb6f0b519674e5d2b9c331b32ff677592
SamanthaSutter <111908649+SamanthaSutter@users.noreply.github.com>
2023-06-25 15:22:00 -0500

    Commit

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import { Console } from 'console';
 import express from 'express';
 import connectDatabase from './config/db';
 
@@ -11,6 +12,27 @@ connectDatabase();
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
+
+//Configure middleware
+app.use(express.json({ extended: false }));
+
+//API endpoints
+/**
+ * @route GET/
+ * @desc Test endpoint
+ */
+app.get('/', (req, res) =>
+    res.send('http get request sent to root api endpoint')
+);
+
+/**
+ * @route POST api/users
+ * @desc Register user
+ */
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 //Connection listener
 app.listen(300, () => console.log('Express server running on port 3000'));
